dbb395094d15efd13c9cefa2383681382f74d91b
Rito250693 <ritoerdocarrgar@gmail.com>
2022-10-18 19:06:52 -0500

    README.md p69-tabla-converciones.py

--- p69-tabla-converciones.py
+++ p69-tabla-converciones.py
@@ -14,6 +14,6 @@ print(f'{" ":-^45}')
 print(f'....Decimal____Exa____Octal_______Binario.....')
 print(f'{" ":-^45}')
 for i in range(inicio,final+1):
-    print(f"|{i:>10}|{i:^10}|{i:<10o}|{i:>10b}|")
+    print(f"|{i:>10}|{i:^10x}|{i:<10o}|{i:>10b}|")
 
 print(f'{" ":-^45}')
